696b555167742ece169571cd911e9d9d0233f885
Ricardo Sanchez <nardove@yahoo.com>
2021-03-03 08:12:26 +0000

    Part 6 additions

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -10,6 +10,18 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        # Check if user exists in DB
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That user name is taken. Please choose a different one')
+    
+    def validate_email(self, email):
+        # Check if user exists in DB
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email name is taken. Please choose a different one')
 
 
 class LoginForm(FlaskForm):
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,24 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
+from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flaskblog import app
+from flask_login import login_user, current_user, logout_user, login_required
 
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
@@ -17,9 +33,19 @@ def about():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account create for {form.username.data}!', 'success')
+        # Hash password from form
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        # Create new user
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        # Save user data to DB
+        db.session.add(user)
+        db.session.commit()
+        # Display success message
+        flash(f'Your account has been created, you can now login', 'success')
         return redirect(url_for('home'))
 
     return render_template('register.html', title='Register', form=form)
@@ -27,12 +53,28 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash(f'You have been logged in!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash(f'Login unsuccessful. Please check username and password', 'danger')
+            flash(f'Login unsuccessful. Please check email and password', 'danger')
 
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='Account')
