63a9149d052c0d47466d8d3c4431891e657633c6
Ricardo Sanchez <nardove@yahoo.com>
2021-03-08 19:32:48 +0000

    Part 10 completed

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,16 +1,30 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ccac162550f28abf4bd7d01444e56745'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config["SECRET_KEY"] = "ccac162550f28abf4bd7d01444e56745"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["MAIL_SERVER"] = "smtp.mail.yahoo.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+# app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
+app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASS_TEMP")
+print(os.environ.get("EMAIL_USER"))
+print(os.environ.get("EMAIL_PASS_TEMP"))
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+
+mail = Mail(app)
 
 from flaskblog import routes
